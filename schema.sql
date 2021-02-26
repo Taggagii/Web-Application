5f42219f9260be42e8856bf97e5954d6c87a56ac
Joe-Joe-Joe-Joe <joedolina@gmail.com>
2021-02-26 11:32:18 -0500

    Completed the DB schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,15 @@
 drop table if exists weather;
     create table weather (
     id integer primary key autoincrement,
-    )
+    city text not null,
+    country text not null,
+    lat decimal(6, 3) not null,
+    lon decimal(6, 3) not null,
+    last_call datetime,
+    temperature integer not null,
+    condition text not null,
+    warnings text,
+    precipitation integer not null,
+    high integer not null,
+    low integer not null
+    );
