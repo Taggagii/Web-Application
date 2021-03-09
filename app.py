0a8f63f7c50cfa256328ac46ac39722e520a5c40
Joe <joedolina@gmail.com>
2021-03-08 19:05:00 -0500

    Making everything look nice and professional

--- app.py
+++ app.py
@@ -5,23 +5,28 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 import re
+from packages.weather import WeatherRequest
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) #josh if you move the database I will hunt you down mark my words
 
-page_link_dict = {"Home": "/",
-        "About" : "/about",
-        "Pseudocode": "/pseudocode",
-        "Weather": "/weather",
-        "Polls": "/polls"
+page_link_dict = {
+        "Home": "/home",
+        "About" : "/about/",
+        "Pseudocode": "/pseudocode/",
+        "Weather": "/weather/",
+        "Polls": "/polls/"
                   }
 
 
-@app.route("/")
+@app.route("/") #Just a redirect for the default enpoint. All homepage changes should be made to /home/
 def default():
     return redirect("/home/")
 
+
+# All endpoint returns should follow the format return("<current page>.html", pages=page_link_dict,
+# currentPage="<current page>" dictionary_of_parameters)
 
 @app.route("/home/")
 def index():
@@ -35,13 +40,13 @@ def about():
 
 @app.route("/pseudocode/")
 def pseudocode():
-    return render_template('pseudocode.html', pages = page_link_dict, currentPage="Pseudocode")
+    return render_template('pseudocode.html', pages=page_link_dict, currentPage="Pseudocode")
 
 
 @app.route("/weather/", methods = ['GET'])
 def weather():
-
-    return render_template("weather.html", pages=page_link_dict, currentPage="Weather")
+    weather_dict = {}
+    return render_template("weather.html", pages=page_link_dict, currentPage="Weather", weather=weather_dict)
 
 
 @app.route("/polls/")
